9a0128ba566153a418c88be39bcb9eaafa860642
mane770 <153079962+mane770@users.noreply.github.com>
2024-02-25 15:14:17 +0400

    Cosnsole.js

---
+++ cosnsole.js
@@ -0,0 +1,13 @@
+document.querySelector('#loginBtn');
+
+document.querySelector('#loginBtn')
+  .textContent
+
+document.querySelector('#loginBtn')
+  .innerText
+document.querySelector('#loginBtn')
+  .textContent = 'You are loggen in';
+
+document.querySelector('#state')
+  .text.Content = 'Welcome to your account';
+
